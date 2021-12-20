5aaaab0332bd3d6d8879bc912cc5ca6066c40898
JakubReha <kubareha107@gmail.com>
2021-12-20 13:13:38 +0100

    fixed image prep

--- image_prep.py
+++ image_prep.py
@@ -12,32 +12,26 @@ dim = 50
 numb_samples = 100
 samples = []
 
+
 for image in data_images:
     x, y = image.size
 
     for _ in range(numb_samples):
-        x1 = random.randrange(0, x - dim)
-        y1 = random.randrange(0, y - dim)
+        x1 = random.randrange(0, x - dim - 1)
+        y1 = random.randrange(0, y - dim - 1)
+        sample = image.crop((x1, y1, x1 + dim, y1 + dim))
+        samples.append(sample)
+        #sample.show()
 
-        samples.append(im.crop((x1, y1, x1 + dim, y1 + dim)))
-
-to_remove = set(random.sample(range(len(samples)), 300))
-# 1000 samples
-samples = [x for i, x in enumerate(samples) if not i in to_remove]
-
-samples[0].show()
-
-# 1 d arrays
 flat_arrays = []
-# salt and pepper added
 saltpep_arrays = []
 for image in samples:
     image_arr = np.array(image)
     flat_arr = image_arr.reshape(-1)
     flat_arrays.append(flat_arr)
 
-    for pixel in flat_arr:
-        proba = random.randrange(1,100)
+    for pixel in range(len(flat_arr)):
+        proba = random.randrange(1, 100)
         if proba <= 20:
             flip = random.randint(0, 1)
             if flip == 0:
@@ -47,5 +41,5 @@ for image in samples:
     saltpep_arrays.append(flat_arr)
 
 
-normal_im = Image.fromarray(flat_arrays[3].reshape(50,50))
+normal_im = Image.fromarray(flat_arrays[3].reshape(50, 50))
 normal_im.show()
