normalization for inner product, running time fixes

DCT.py
@@ -1,6 +1,5 @@
 import numpy as np
 from DimRed import DimRed
-from tqdm import tqdm
 
 class DCT(DimRed):
     def __init__(self, X):
@@ -9,12 +8,6 @@ class DCT(DimRed):
         self.d = X.shape[0]
         self.scale = 1
         self.closest_power = 1
-        type = "numpy"
-        self.y = np.empty_like(self.X, dtype=float)
-        for i in range(len(self.X.T)):
-            self.y[:, i] = self.dctf(self.X[:, i], type)
-        """while self.closest_power < self.d:
-            self.closest_power *= 2"""
 
 
     def fft(self, samples):
@@ -25,37 +18,40 @@ class DCT(DimRed):
             even_samples = self.fft(samples[::2])
             odd_samples = self.fft(samples[1::2])
             multiplicative_term = np.exp(-2j * np.pi * np.arange(N) / N)
-
             X = np.concatenate([even_samples + multiplicative_term[:int(N / 2)] * odd_samples,even_samples + multiplicative_term[int(N / 2):] * odd_samples])
             return X
 
-    def dctf(self, samples, type):
-        dct = []
+    def dctf(self, samples):
         N = self.d
-        if type == "numpy":
-            x = np.zeros(N * 2)
-            x[:N] = samples
-            x[N:] = samples[::-1]
-            dct = np.fft.fft(x)[:N]
-        elif type == "classic":
-            N = self.closest_power
-            N_zeros = N - self.d
-            samples = np.pad(samples, (0, N_zeros), 'constant')
-            x = np.zeros(N*2)
-            x[:N] = samples
-            x[N:] = samples[::-1]
-            dct = self.fft(x)[:N]
-        else:
-            raise Exception("specify the the type of fft")
-
+        x = np.zeros(N * 2)
+        x[:N] = samples
+        x[N:] = samples[::-1]
+        dct = np.fft.fft(x)[:N]
         dct[0] = dct[0] * 1/(2*np.sqrt(N))
         i = np.arange(1, len(dct))
         dct[1:] = dct[1:] * 1/(np.sqrt(2*N))*np.exp(-1j * np.pi * i / (2 * N))
-
         return dct.real
 
-    def fit(self, k):
+    def fit(self, k, m):
+        self.ids1 = np.random.choice(range(self.N), size=m)
+        self.ids2 = np.random.choice(list(set(range(self.N)) - set(self.ids1)), size=m)
+        self.y = np.empty_like(self.X, dtype=float)
+
+        for i in self.ids1:
+            self.y[:, i] = self.dctf(self.X[:, i])
+        for i in self.ids2:
+            self.y[:, i] = self.dctf(self.X[:, i])
         self.k = k
         self.X_k = self.y[:self.k]
         return self.X_k
+
+    def distortions_euclidean(self):
+        """
+        for image data
+        :param m: number of pair comparisons
+        :return: distortions measured by euclidean distance
+        """
+        emd_dist = self.scale * np.sqrt(np.sum((self.X_k.T[self.ids1] - self.X_k.T[self.ids2])**2, axis=1))
+        dist = np.sqrt(np.sum((self.X.T[self.ids1] - self.X.T[self.ids2])**2, axis=1))
+        return emd_dist - dist
 

main.py
@@ -84,8 +84,8 @@ def test_text():
         y_srp[i] = srp.distortions_inner_product(pairs)"""
         pca.fit(k)
         y_pca[i] = pca.distortions_inner_product(pairs)
-        """dct.fit(k)
-        y_dct[i] = dct.distortions_inner_product(pairs)"""
+        """dct.fit(k, pairs)
+        y_dct[i] = dct.distortions_inner_product()"""
         rp = RP(X, k)
         rp.fit()
         y_rp[i] = rp.distortions_inner_product(pairs)
@@ -106,7 +106,7 @@ def test_text():
     plt.legend(["RP", "PCA"])
     plt.title("Average error using RP and SVD")
     plt.xlabel("Reduced dim. of data")
-    plt.ylabel("Error difference")
+
     plt.show()
 
 def test_image(type="img"):
@@ -147,8 +147,8 @@ def test_image(type="img"):
         y_srp[i] = srp.distortions_euclidean(pairs)
         pca.fit(k)
         y_pca[i] = pca.distortions_euclidean(pairs)
-        dct.fit(k)
-        y_dct[i] = dct.distortions_euclidean(pairs)
+        dct.fit(k, pairs)
+        y_dct[i] = dct.distortions_euclidean()
         rp = RP(X, k)
         rp.fit()
         y_rp[i] = rp.distortions_euclidean(pairs)
@@ -171,12 +171,11 @@ def test_image(type="img"):
     if type == "noisy_img":
         plt.fill_between(x, (np.mean(y_mf) - ci_mf), (np.mean(y_mf) + ci_mf), color='black', alpha=0.1)
         plt.legend(["MF", "RP", "PCA", "SRP", "DCT"])
-        plt.title("Average error using MF, PCA, RP, SVD and DCT")
+        plt.title("Average error using MF, PCA, RP, SRP and DCT")
     else:
         plt.legend(["RP", "PCA", "SRP", "DCT"])
-        plt.title("Average error using PCA, RP, SVD and DCT")
+        plt.title("Average error using PCA, RP, SRP and DCT")
     plt.xlabel("Reduced dim. of data")
-    plt.ylabel("Error difference")
     plt.show()
 
 def test_audio():
@@ -216,8 +215,8 @@ def test_audio():
         rp.fit()
         y_rp[i] = rp.distortions_euclidean(pairs)
     #for i, k in enumerate(tqdm(range(start, stop, step))):
-        dct.fit(k)
-        y_dct[i] = dct.distortions_euclidean(pairs)
+        dct.fit(k, pairs)
+        y_dct[i] = dct.distortions_euclidean()
 
     #x_dct = np.arange(start, stop, step)
     x = np.arange(start2, stop2, step2)
@@ -249,13 +248,12 @@ def test_audio():
     plt.fill_between(x, (np.mean(y_dct, axis=1) - ci_dct), (np.mean(y_dct, axis=1) + ci_dct), color='green', alpha=0.1)
 
     plt.legend(["RP", "PCA", "SRP", "DCT"])
-    plt.title("Average error using PCA, RP, SVD and DCT")
+    plt.title("Average error using PCA, RP, SRP and DCT")
     plt.xlabel("Reduced dim. of data")
-    plt.ylabel("Error difference")
     plt.show()
 
 
-def text2():
+def test_text2():
     docs = glob.glob('data/text2/**/*')
     corpus = []
     for doc in docs:
@@ -283,8 +281,8 @@ def text2():
         y_srp[i] = srp.distortions_inner_product(pairs)"""
         pca.fit(k)
         y_pca[i] = pca.distortions_inner_product(pairs)
-        """dct.fit(k)
-        y_dct[i] = dct.distortions_inner_product(pairs)"""
+        """dct.fit(k, pairs)
+        y_dct[i] = dct.distortions_inner_product()"""
         rp = RP(X, k)
         rp.fit()
         y_rp[i] = rp.distortions_inner_product(pairs)
@@ -305,13 +303,12 @@ def text2():
     plt.legend(["RP", "PCA"])
     plt.title("Average error using RP and SVD")
     plt.xlabel("Reduced dim. of data")
-    plt.ylabel("Error difference")
     plt.show()
 
 
 if __name__ == "__main__":
-    #text2()
-    test_image("img")
+    test_text()
+    #test_image("img")
     #test_audio()
     #test_image("noisy_img")
-    #test_text()
+    test_text2()

rp.py
@@ -1,6 +1,5 @@
 import numpy as np
 from DimRed import DimRed
-from sklearn.random_projection import GaussianRandomProjection as GRP
 
 class RP(DimRed):
     def __init__(self, X, k):
@@ -13,7 +12,7 @@ class RP(DimRed):
         self.d = X.shape[0]
         self.scale = np.sqrt(self.d / self.k)
         self.X = X
-        self.R = np.random.normal(loc=0.0, scale=1, size=((self.d, self.k)))
+        self.R = np.random.normal(loc=0.0, scale=1.0, size=((self.d, self.k)))
         self.R = (self.R / np.sqrt((self.R**2).sum(axis=0))).T
 
     def fit(self):
@@ -24,6 +23,16 @@ class RP(DimRed):
         self.X_k = self.R @ self.X
         return self.X_k
 
-
+    def distortions_inner_product(self, m):
+        """
+        for text data
+        :param m: number of pair comparisons
+        :return: distortions measured by inner product
+        """
+        ids1 = np.random.choice(range(self.N), size=m)
+        ids2 = np.random.choice(list(set(range(self.N))-set(ids1)), size=m)
+        emd_prod = self.scale**2 * np.sum(np.multiply(self.X_k.T[ids1], self.X_k.T[ids2]), axis=1)
+        prod = np.sum(np.multiply(self.X.T[ids1], self.X.T[ids2]), axis=1)
+        return emd_prod - prod
 
 

srp.py
@@ -1,6 +1,6 @@
 import numpy as np
 from DimRed import DimRed
-from sklearn.random_projection import SparseRandomProjection as SRP2
+
 class SRP(DimRed):
     def __init__(self, X, k):
         """
@@ -12,8 +12,7 @@ class SRP(DimRed):
         self.d = X.shape[0]
         self.X = X
         self.scale = np.sqrt(self.d / self.k)
-        R = np.sqrt(3) * np.random.choice([1, 0, -1], size=self.k * self.d, p=[1 / 6, 2 / 3, 1 / 6])
-        self.R = R.reshape((self.d, self.k))
+        self.R = np.sqrt(3) * np.random.choice([1, 0, -1], size=((self.d, self.k)), p=[1 / 6, 2 / 3, 1 / 6])
         self.R = (self.R / np.sqrt((self.R ** 2).sum(axis=0))).T
 
     def fit(self):